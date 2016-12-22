b1995df29f7bf7530231e66f61347e744a2ad9c3
wickersoft <wickersoft@online.de>
2016-12-22 09:57:20 +0100

    Deduplicated adapter code

--- NMS_1_11_R1/src/main/java/zedly/zenchantments/compatibility/NMS_1_11_R1.java
+++ NMS_1_11_R1/src/main/java/zedly/zenchantments/compatibility/NMS_1_11_R1.java
@@ -17,31 +17,18 @@ import net.minecraft.server.v1_11_R1.EnumHand;
 import net.minecraft.server.v1_11_R1.PacketDataSerializer;
 import net.minecraft.server.v1_11_R1.PacketPlayOutEntityDestroy;
 import net.minecraft.server.v1_11_R1.PacketPlayOutSpawnEntityLiving;
-import org.bukkit.craftbukkit.v1_11_R1.block.CraftBlockState;
 import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_11_R1.entity.CraftMushroomCow;
 import org.bukkit.craftbukkit.v1_11_R1.entity.CraftSheep;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.BlockState;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.block.EntityBlockFormEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.entity.EntityCombustByEntityEvent;
 import static org.bukkit.Material.*;
 import org.bukkit.entity.EntityType;
 import static org.bukkit.entity.EntityType.*;
 
-import org.bukkit.inventory.ItemStack;
 
 /**
  *
@@ -126,43 +113,6 @@ public class NMS_1_11_R1 extends CompatibilityAdapter {
         return success;
     }
 
-    /**
-     * Places a block on the given player's behalf. Fires a BlockPlaceEvent with
-     * (nearly) appropriate parameters to probe the legitimacy (permissions etc)
-     * of the action and to communicate to other plugins where the block is
-     * coming from.
-     *
-     * @param blockPlaced the block to be changed
-     * @param player the player whose identity to use
-     * @param mat the material to set the block to, if allowed
-     * @param blockData the block data to set for the block, if allowed
-     * @return true if the block placement has been successful
-     */
-    @Override
-    public boolean placeBlock(Block blockPlaced, Player player, Material mat, int blockData) {
-        Block blockAgainst = blockPlaced.getRelative((blockPlaced.getY() == 0) ? BlockFace.UP : BlockFace.DOWN);
-        ItemStack itemHeld = new ItemStack(mat, 1, (short) blockData);
-        BlockPlaceEvent placeEvent = new BlockPlaceEvent(blockPlaced, blockAgainst.getState(), blockAgainst, itemHeld, player, true);
-        Bukkit.getPluginManager().callEvent(placeEvent);
-        if (!placeEvent.isCancelled()) {
-            blockPlaced.setType(mat);
-            blockPlaced.setData((byte) blockData);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean attackEntity(LivingEntity target, Player attacker, double damage) {
-        EntityDamageByEntityEvent damageEvent = new EntityDamageByEntityEvent(attacker, target, DamageCause.ENTITY_ATTACK, damage);
-        Bukkit.getPluginManager().callEvent(damageEvent);
-        if (!damageEvent.isCancelled()) {
-            target.damage(damage);
-            return true;
-        }
-        return false;
-    }
-
     @Override
     public boolean shearEntityNMS(Entity target, Player player, boolean mainHand) {
         if (target instanceof CraftSheep) {
@@ -171,66 +121,6 @@ public class NMS_1_11_R1 extends CompatibilityAdapter {
         } else if (target instanceof CraftMushroomCow) {
             EntityMushroomCow entityMushroomCow = ((CraftMushroomCow) target).getHandle();
             return entityMushroomCow.a(((CraftPlayer) player).getHandle(), mainHand ? EnumHand.MAIN_HAND : EnumHand.OFF_HAND);
-        }
-        return false;
-    }
-
-    @Override
-    public boolean haulOrBreakBlock(Block from, Block to, BlockFace face, Player player) {
-        BlockState state = from.getState();
-        if (state.getClass() != CraftBlockState.class) {
-            return false;
-        }
-        BlockBreakEvent breakEvent = new BlockBreakEvent(from, player);
-        Bukkit.getPluginManager().callEvent(breakEvent);
-        if (breakEvent.isCancelled()) {
-            return false;
-        }
-        ItemStack stack = new ItemStack(state.getType(), 1, state.getData().getData());
-        from.setType(Material.AIR);
-        from.setData((byte) 0);
-        BlockPlaceEvent placeEvent = new BlockPlaceEvent(to, to.getRelative(face.getOppositeFace()).getState(), to.getRelative(face.getOppositeFace()), stack, player, true);
-        Bukkit.getPluginManager().callEvent(placeEvent);
-        if (placeEvent.isCancelled()) {
-            from.getWorld().dropItem(from.getLocation(), stack);
-            return true;
-        }
-        to.setType(state.getType());
-        to.setData(state.getData().getData());
-        return true;
-    }
-
-    @Override
-    public boolean igniteEntity(Entity target, Player player, int duration) {
-        EntityCombustByEntityEvent evt = new EntityCombustByEntityEvent(target, player, duration);
-        Bukkit.getPluginManager().callEvent(evt);
-        if (!evt.isCancelled()) {
-            target.setFireTicks(duration);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean damagePlayer(Player player, double damage, DamageCause cause) {
-        EntityDamageEvent evt = new EntityDamageEvent(player, cause, damage);
-        Bukkit.getPluginManager().callEvent(evt);
-        if (!evt.isCancelled()) {
-            player.setLastDamageCause(evt);
-            player.damage(damage);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean formBlock(Block block, Material mat, byte data, Player player) {
-        EntityBlockFormEvent evt = new EntityBlockFormEvent(player, block, new MockBlockState(block, Material.FROSTED_ICE, (byte) 0));
-        Bukkit.getPluginManager().callEvent(evt);
-        if (!evt.isCancelled()) {
-            block.setType(mat);
-            block.setData(data);
-            return true;
         }
         return false;
     }
