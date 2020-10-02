1a22518148f90bb741e263751a2b499daa442361
Louis Salkeld <louissalkeld@outlook.com>
2020-10-02 01:06:37 +0100

    Clean-up code in GrindstoneMerge

--- src/main/java/zedly/zenchantments/GrindstoneMerge.java
+++ src/main/java/zedly/zenchantments/GrindstoneMerge.java
@@ -1,28 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package zedly.zenchantments;
 
-import java.util.Map;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
-import static org.bukkit.event.EventPriority.MONITOR;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.GrindstoneInventory;
 import org.bukkit.inventory.ItemStack;
 
+import static org.bukkit.event.EventPriority.MONITOR;
+
 /**
- *
  * @author Dennis
  */
 public class GrindstoneMerge implements Listener {
-
     private static final GrindstoneMerge INSTANCE = new GrindstoneMerge();
 
     public static GrindstoneMerge instance() {
@@ -31,27 +24,35 @@ public class GrindstoneMerge implements Listener {
 
     @EventHandler(priority = MONITOR)
     public void onClicks(final InventoryClickEvent evt) {
-        if (evt.getInventory().getType() == InventoryType.GRINDSTONE) {
-            GrindstoneInventory gi = (GrindstoneInventory) evt.getInventory();
-            World world = (World) evt.getViewers().get(0).getWorld();
-            if (evt.getSlot() == 2) {
-                removeOutputEnchants(gi, world);
-            } else {
-                Bukkit.getScheduler().scheduleSyncDelayedTask(Storage.zenchantments, () -> {
-                    removeOutputEnchants(gi, world);
-                }, 0);
-            }
+        if (evt.getInventory().getType() != InventoryType.GRINDSTONE) {
+            return;
+        }
+
+        GrindstoneInventory inventory = (GrindstoneInventory) evt.getInventory();
+        World world = evt.getViewers().get(0).getWorld();
+
+        if (evt.getSlot() == 2) {
+            this.removeOutputEnchants(inventory, world);
+        } else {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(
+                Storage.zenchantments,
+                () -> this.removeOutputEnchants(inventory, world),
+                0
+            );
         }
     }
 
-    private void removeOutputEnchants(GrindstoneInventory gi, World world) {
-        if (gi.getItem(2) != null && gi.getItem(2).getType() != Material.AIR) { // Get the output item
-            ItemStack stk = gi.getItem(2);
-            Map<CustomEnchantment, Integer> enchants = CustomEnchantment.getEnchants(stk, world);
-            for (CustomEnchantment ench : enchants.keySet()) {
-                CustomEnchantment.setEnchantment(stk, ench, 0, world);
-            }
-            gi.setItem(2, stk);
+    private void removeOutputEnchants(GrindstoneInventory inventory, World world) {
+        final ItemStack itemStack = inventory.getItem(2);
+
+        if (itemStack == null || itemStack.getType() == Material.AIR) {
+            return;
         }
+
+        for (CustomEnchantment enchantment : CustomEnchantment.getEnchants(itemStack, world).keySet()) {
+            CustomEnchantment.setEnchantment(itemStack, enchantment, 0, world);
+        }
+
+        inventory.setItem(2, itemStack);
     }
 }
