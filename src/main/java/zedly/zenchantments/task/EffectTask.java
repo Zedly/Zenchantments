b8a9c0ca2a671749c838c9d8ff365c504d07a406
Louis Salkeld <louissalkeld@outlook.com>
2020-10-02 01:54:15 +0100

    Minor clean-up of EffectTask

--- src/main/java/zedly/zenchantments/task/EffectTask.java
+++ src/main/java/zedly/zenchantments/task/EffectTask.java
@@ -6,10 +6,9 @@ import java.lang.annotation.*;
  * Method annotation used by {@link TaskRunner} to control frequency of execution of scheduled
  * events. Annotations must only be on static methods.
  */
-
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface EffectTask {
-	Frequency value();
+    Frequency value();
 }
