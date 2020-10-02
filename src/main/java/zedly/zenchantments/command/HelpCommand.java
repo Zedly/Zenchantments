45738f43e49ead25019603bf7301b013c7631f74
Louis Salkeld <louissalkeld@outlook.com>
2020-10-02 18:51:01 +0100

    Implement HelpCommand

--- src/main/java/zedly/zenchantments/command/HelpCommand.java
+++ src/main/java/zedly/zenchantments/command/HelpCommand.java
@@ -1,6 +1,8 @@
 package zedly.zenchantments.command;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import zedly.zenchantments.Storage;
 import zedly.zenchantments.ZenchantmentsPlugin;
 
 import java.util.Collections;
@@ -13,7 +15,43 @@ public class HelpCommand extends ZenchantmentsCommand {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-
+        sender.sendMessage(Storage.logo);
+        sender.sendMessage(
+            ChatColor.DARK_AQUA
+                + "- ench info <?enchantment>: "
+                + ChatColor.AQUA
+                + "Returns information about custom enchantments."
+        );
+        sender.sendMessage(
+            ChatColor.DARK_AQUA
+                + "- ench list: "
+                + ChatColor.AQUA
+                + "Returns a list of enchantments for the tool in hand."
+        );
+        sender.sendMessage(
+            ChatColor.DARK_AQUA
+                + "- ench give <Player> <Material> <enchantment> <?level> ... "
+                + ChatColor.AQUA
+                + "Gives the target a specified enchanted item."
+        );
+        sender.sendMessage(
+            ChatColor.DARK_AQUA
+                + "- ench <enchantment> <?level>: "
+                + ChatColor.AQUA
+                + "Enchants the item in hand with the given enchantment and level"
+        );
+        sender.sendMessage(
+            ChatColor.DARK_AQUA
+                + "- ench disable <enchantment/all>: "
+                + ChatColor.AQUA
+                + "Disables selected enchantment for the user"
+        );
+        sender.sendMessage(
+            ChatColor.DARK_AQUA
+                + "- ench enable <enchantment/all>: "
+                + ChatColor.AQUA
+                + "Enables selected enchantment for the user"
+        );
     }
 
     @Override
