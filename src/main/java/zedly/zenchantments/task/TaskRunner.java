9386c7beeb8f7657b93ed1ad7652a2d0d9ea9991
Louis Salkeld <louissalkeld@outlook.com>
2020-10-02 01:50:34 +0100

    Clean-up code in TaskRunner

--- src/main/java/zedly/zenchantments/task/TaskRunner.java
+++ src/main/java/zedly/zenchantments/task/TaskRunner.java
@@ -15,38 +15,42 @@ import java.util.logging.Logger;
 
 /**
  * A runnable class that will execute all events of the specified frequency.
- *
  */
 public class TaskRunner implements Runnable {
-
-    private Set<Method> tasks;
-    private Logger logger;
+    private final Set<Method> tasks;
+    private final Logger      logger;
 
     /**
      * Initializes this EventRunner by collecting all methods with an
      * {@link EffectTask} annotation of the specified frequency.
      *
-     * @param freq The frequency of annotation that we'll be running.
+     * @param frequency The frequency of annotation that we'll be running.
      */
-    public TaskRunner(Frequency freq) {
+    public TaskRunner(Frequency frequency) {
         this.logger = Bukkit.getLogger();
+        this.tasks = new HashSet<>();
 
-        tasks = new HashSet<>();
+        new FastClasspathScanner(Zenchantments.class.getPackage().getName())
+            .overrideClasspath(Storage.pluginPath)
+            .matchClassesWithMethodAnnotation(
+                EffectTask.class,
+                (clazz, method) -> {
+                    if (!Modifier.isStatic(method.getModifiers())) {
+                        this.logger.warning(
+                            "EffectTask on non-static method '" + method.getName() + "' in class '" + clazz.getName() + "'."
+                        );
+                    }
 
-        new FastClasspathScanner(Zenchantments.class.getPackage().getName()).overrideClasspath(Storage.pluginPath)
-                .matchClassesWithMethodAnnotation(
-                        EffectTask.class,
-                        (clazz, method) -> {
-                            if (!Modifier.isStatic(method.getModifiers())) {
-                                this.logger.warning(
-                                        "EffectTask on non-static method '" + method.getName() + "' in class '"
-                                        + clazz.getName() + "'");
-                            }
-                            if (method.getAnnotation(EffectTask.class).value() == freq) {
-                                assert method instanceof Method : "Event annotation not valid on constructors";
-                                tasks.add((Method) method);
-                            }
-                        }).scan();
+                    if (method.getAnnotation(EffectTask.class).value() == frequency) {
+                        if (!(method instanceof Method)) {
+                            throw new IllegalStateException("EffectTask annotation not valid on constructors.");
+                        }
+
+                        tasks.add((Method) method);
+                    }
+                }
+            )
+            .scan();
     }
 
     /**
@@ -58,13 +62,16 @@ public class TaskRunner implements Runnable {
      */
     @Override
     public void run() {
-        for (Method m : tasks) {
+        for (Method method : this.tasks) {
             try {
-                m.invoke(null);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                this.logger.log(Level.SEVERE, "Could not invoke event '" + m.getName() + "' due to \n" + e.getCause(),
-                        e);
-                e.printStackTrace();
+                method.invoke(null);
+            } catch (IllegalAccessException | InvocationTargetException ex) {
+                this.logger.log(
+                    Level.SEVERE,
+                    "Could not invoke event '" + method.getName() + "' due to " + ex.getCause(),
+                    ex
+                );
+                ex.printStackTrace();
             }
         }
     }
