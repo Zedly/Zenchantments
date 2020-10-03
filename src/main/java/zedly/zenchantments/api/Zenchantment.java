5e32c2e8d1b6792cb49890045625cafe79e856eb
Louis Salkeld <louissalkeld@outlook.com>
2020-10-03 06:36:34 +0100

    Begin documenting API interfaces

--- src/main/java/zedly/zenchantments/api/Zenchantment.java
+++ src/main/java/zedly/zenchantments/api/Zenchantment.java
@@ -3,24 +3,86 @@ package zedly.zenchantments.api;
 import zedly.zenchantments.Hand;
 import zedly.zenchantments.Tool;
 
+/**
+ * Represents a zenchantment.
+ */
 public interface Zenchantment {
+    /**
+     * Gets the identifier of this {@link Zenchantment}.
+     *
+     * @return An {@code int} representing the identifier of this {@link Zenchantment}.
+     */
     int getId();
 
+    /**
+     * Gets the name of this {@link Zenchantment}.
+     * <p>
+     * This is also the text used in the lore of an item enchanted with it.
+     *
+     * @return A {@link String} representing the name of this {@link Zenchantment}.
+     */
     String getLoreName();
 
+    /**
+     * Gets the description of this {@link Zenchantment}.
+     * <p>
+     * Depending on the configuration, this can appear in the lore of an item enchanted with it beneath the name/level.
+     *
+     * @return A {@link String} representing the description of this {@link Zenchantment}.
+     */
     String getDescription();
 
+    /**
+     * Gets the maximum possible level this {@link Zenchantment} can naturally obtain.
+     *
+     * @return An {@code int} representing the maximum possible level this {@link Zenchantment} can naturally obtain.
+     */
     int getMaxLevel();
 
+    /**
+     * Gets the cooldown (in ticks) for this {@link Zenchantment}.
+     * <p>
+     * The default value is 0.
+     *
+     * @return An {@code int} representing the cooldown (in ticks) for this {@link Zenchantment}.
+     */
     int getCooldown();
 
+    /**
+     * Gets the power multiplier for the effects of this {@link Zenchantment}.
+     * <p>
+     * The default value is 0. A value of -1 means there will be no effects.
+     *
+     * @return A {@code double} representing the power multiplier for the effects of this {@link Zenchantment}.
+     */
     double getPower();
 
+    /**
+     * Gets the relative probability of obtaining this {@link Zenchantment}.
+     *
+     * @return A {@code float} representing the relative probability of obtaining this {@link Zenchantment}.
+     */
     float getProbability();
 
+    /**
+     * Gets the {@link Tool Tools} that can receive and work with this {@link Zenchantment}.
+     *
+     * @return An array of {@link Tool Tools} that can receive and work with this {@link Zenchantment}.
+     */
     Tool[] getEnchantable();
 
+    /**
+     * Gets the {@link Class Classes} of {@link Zenchantment Zenchantments} that conflict with this
+     * {@link Zenchantment}.
+     *
+     * @return An array of {@link Class Classes} of {@link Zenchantments} that conflict with this {@link Zenchantment}.
+     */
     Class<?>[] getConflicting();
 
+    /**
+     * Gets the hands that this {@link Zenchantment} has actions for.
+     *
+     * @return A {@link Hand} value representing the hands that this {@link Zenchantment} has actions for.
+     */
     Hand getHandUse();
 }
--- src/main/java/zedly/zenchantments/api/Zenchantments.java
+++ src/main/java/zedly/zenchantments/api/Zenchantments.java
@@ -5,8 +5,23 @@ import zedly.zenchantments.api.player.PlayerDataProvider;
 
 import java.util.Set;
 
+/**
+ * Represents the main Zenchantments plugin class and entry-point into the API.
+ */
 public interface Zenchantments {
+    /**
+     * Gets the {@link PlayerDataProvider} of this {@link Zenchantments} instance.
+     *
+     * @return The {@link PlayerDataProvider} of this {@link Zenchantments} instance.
+     */
     PlayerDataProvider getPlayerDataProvider();
 
+    /**
+     * Gets the {@link Zenchantment Zenchantments} enabled for the given {@link World}.
+     *
+     * @param world The {@link World} to get the enabled {@link Zenchantment Zenchantments} for.
+     *
+     * @return A {@link Set} of the enabled {@link Zenchantment Zenchantments}.
+     */
     Set<Zenchantment> getZenchantmentsForWorld(World world);
 }
