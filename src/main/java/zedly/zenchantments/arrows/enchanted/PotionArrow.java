ad1722b438e4023af026a755a04be29d1bd2234b
Louis Salkeld <louissalkeld@outlook.com>
2020-10-08 21:28:17 +0100

    Fix and clean-up PotionArrow

--- src/main/java/zedly/zenchantments/arrows/enchanted/PotionArrow.java
+++ src/main/java/zedly/zenchantments/arrows/enchanted/PotionArrow.java
@@ -3,27 +3,31 @@ package zedly.zenchantments.arrows.enchanted;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 import zedly.zenchantments.Storage;
 import zedly.zenchantments.Utilities;
 import zedly.zenchantments.arrows.EnchantedArrow;
 
-import static org.bukkit.potion.PotionEffectType.*;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class PotionArrow extends EnchantedArrow {
 
-	public PotionArrow(Arrow entity, int level, double power) {
-		super(entity, level, power);
-	}
+    public PotionArrow(Arrow entity, int level, double power) {
+        super(entity, level, power);
+    }
 
-	public boolean onImpact(@NotNull EntityDamageByEntityEvent event) {
-		if (Storage.rnd.nextInt((int) Math.round(10 / (getLevel() * getPower() + 1))) == 1) {
-			Utilities.addPotion((LivingEntity) arrow.getShooter(),
-				Storage.COMPATIBILITY_ADAPTER.PotionPotions().get(Storage.rnd.nextInt(12)),
-				150 + (int) Math.round(getLevel() * getPower() * 50), (int) Math.round(getLevel() * getPower()));
-		}
-		die();
-		return true;
-	}
+    public boolean onImpact(@NotNull EntityDamageByEntityEvent event) {
+        if (ThreadLocalRandom.current().nextInt((int) Math.round(10 / (this.getLevel() * this.getPower() + 1))) == 1) {
+            Utilities.addPotion(
+                (LivingEntity) Objects.requireNonNull(this.getArrow().getShooter()),
+                Storage.COMPATIBILITY_ADAPTER.PotionPotions().get(ThreadLocalRandom.current().nextInt(12)),
+                150 + (int) Math.round(this.getLevel() * this.getPower() * 50),
+                (int) Math.round(this.getLevel() * this.getPower())
+            );
+        }
+
+        this.die();
+        return true;
+    }
 }
