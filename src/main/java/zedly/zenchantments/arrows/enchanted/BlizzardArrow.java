9b4c847b45cfb859c23daed55c74c392c9d95775
Louis Salkeld <louissalkeld@outlook.com>
2020-10-08 21:09:17 +0100

    Fix and clean-up BlizzardArrow

--- src/main/java/zedly/zenchantments/arrows/enchanted/BlizzardArrow.java
+++ src/main/java/zedly/zenchantments/arrows/enchanted/BlizzardArrow.java
@@ -1,7 +1,10 @@
 package zedly.zenchantments.arrows.enchanted;
 
 import org.bukkit.Particle;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import zedly.zenchantments.Storage;
 import zedly.zenchantments.Utilities;
 import zedly.zenchantments.arrows.EnchantedArrow;
@@ -15,17 +18,33 @@ public class BlizzardArrow extends EnchantedArrow {
     }
 
     public void onImpact() {
-        Utilities.display(Utilities.getCenter(arrow.getLocation()), Particle.CLOUD, 100 * getLevel(), 0.1f,
-                getLevel(), 1.5f, getLevel());
-        double radius = 1 + getLevel() * getPower();
-        for (Entity e : arrow.getNearbyEntities(radius, radius, radius)) {
-            if (e instanceof LivingEntity && !e.equals(arrow.getShooter())
-                    && Storage.COMPATIBILITY_ADAPTER.attackEntity(
-                            (LivingEntity) e, (Player) arrow.getShooter(), 0)) {
-                Utilities.addPotion((LivingEntity) e, SLOW, (int) Math.round(50 + getLevel()
-                        * getPower() * 50), (int) Math.round(getLevel() * getPower() * 2));
+        Utilities.displayParticle(
+            Utilities.getCenter(this.getArrow().getLocation()),
+            Particle.CLOUD,
+            100 * this.getLevel(),
+            0.1f,
+            this.getLevel(),
+            1.5f,
+            this.getLevel()
+        );
+
+        double radius = 1 + this.getLevel() * this.getPower();
+        for (Entity entity : this.getArrow().getNearbyEntities(radius, radius, radius)) {
+            if (!(entity instanceof LivingEntity)
+                || entity.equals(this.getArrow().getShooter())
+                || !Storage.COMPATIBILITY_ADAPTER.attackEntity((LivingEntity) entity, (Player) this.getArrow().getShooter(), 0)
+            ) {
+                continue;
             }
+
+            Utilities.addPotion(
+                (LivingEntity) entity,
+                SLOW,
+                (int) Math.round(50 + this.getLevel() * this.getPower() * 50),
+                (int) Math.round(this.getLevel() * this.getPower() * 2)
+            );
         }
-        die();
+
+        this.die();
     }
 }
