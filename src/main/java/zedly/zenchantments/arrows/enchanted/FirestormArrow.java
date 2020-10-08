6a69f8f3a13b09381cac4deb4aee1031cb873925
Louis Salkeld <louissalkeld@outlook.com>
2020-10-08 21:11:20 +0100

    Fix and clean-up FirestormArrow

--- src/main/java/zedly/zenchantments/arrows/enchanted/FirestormArrow.java
+++ src/main/java/zedly/zenchantments/arrows/enchanted/FirestormArrow.java
@@ -1,7 +1,10 @@
 package zedly.zenchantments.arrows.enchanted;
 
 import org.bukkit.Particle;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import zedly.zenchantments.Storage;
 import zedly.zenchantments.Utilities;
 import zedly.zenchantments.arrows.EnchantedArrow;
@@ -13,16 +16,28 @@ public class FirestormArrow extends EnchantedArrow {
     }
 
     public void onImpact() {
-        Utilities.display(Utilities.getCenter(arrow.getLocation()), Particle.FLAME, 100 * getLevel(), 0.1f,
-                getLevel(), 1.5f, getLevel());
-        double radius = 1 + getLevel() * getPower();
-        for (Entity e : arrow.getNearbyEntities(radius, radius, radius)) {
-            if (e instanceof LivingEntity && !e.equals(arrow.getShooter())
-                    && Storage.COMPATIBILITY_ADAPTER.attackEntity(
-                            (LivingEntity) e, (Player) arrow.getShooter(), 0)) {
-                e.setFireTicks((int) Math.round(getLevel() * getPower() * 100));
+        Utilities.displayParticle(
+            Utilities.getCenter(this.getArrow().getLocation()),
+            Particle.FLAME,
+            100 * this.getLevel(),
+            0.1f,
+            this.getLevel(),
+            1.5f,
+            this.getLevel()
+        );
+
+        double radius = 1 + this.getLevel() * this.getPower();
+        for (Entity entity : this.getArrow().getNearbyEntities(radius, radius, radius)) {
+            if (!(entity instanceof LivingEntity)
+                || entity.equals(this.getArrow().getShooter())
+                || !Storage.COMPATIBILITY_ADAPTER.attackEntity((LivingEntity) entity, (Player) this.getArrow().getShooter(), 0)
+            ) {
+                continue;
             }
+
+            entity.setFireTicks((int) Math.round(this.getLevel() * this.getPower() * 100));
         }
-        die();
+
+        this.die();
     }
 }
