d6aa79c118f2bb9f6254f6b54694834f4ff808f8
Louis Salkeld <louissalkeld@outlook.com>
2020-10-08 21:36:17 +0100

    Fix and clean-up StationaryArrow

--- src/main/java/zedly/zenchantments/arrows/enchanted/StationaryArrow.java
+++ src/main/java/zedly/zenchantments/arrows/enchanted/StationaryArrow.java
@@ -1,12 +1,14 @@
 package zedly.zenchantments.arrows.enchanted;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.*;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityCombustByEntityEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.jetbrains.annotations.NotNull;
 import zedly.zenchantments.Storage;
-import zedly.zenchantments.Zenchantments;
 import zedly.zenchantments.arrows.EnchantedArrow;
 
 public class StationaryArrow extends EnchantedArrow {
@@ -16,32 +18,35 @@ public class StationaryArrow extends EnchantedArrow {
     }
 
     public boolean onImpact(@NotNull EntityDamageByEntityEvent event) {
-        if (Storage.COMPATIBILITY_ADAPTER.attackEntity((LivingEntity) event.getEntity(), (Player) arrow.getShooter(),
-                0)) {
-            LivingEntity ent = (LivingEntity) event.getEntity();
-            if (event.getDamage() < ent.getHealth()) {
+        if (Storage.COMPATIBILITY_ADAPTER.attackEntity((LivingEntity) event.getEntity(), (Player) this.getArrow().getShooter(), 0)) {
+            LivingEntity entity = (LivingEntity) event.getEntity();
+            if (event.getDamage() < entity.getHealth()) {
                 event.setCancelled(true);
 
-                // Imitate Flame arrows after cancelling the original event
-                if (arrow.getFireTicks() > 0) {
-                    EntityCombustByEntityEvent ecbee = new EntityCombustByEntityEvent(arrow, ent, 5);
-                    Bukkit.getPluginManager().callEvent(ecbee);
-                    if (!ecbee.isCancelled()) {
-                        // For some fucking reason I can't set the entity on fire in the same tick. So I'm delaying it by one tick and now it works
-                        Bukkit.getScheduler().scheduleSyncDelayedTask(Storage.zenchantments, () -> {
-                            Storage.COMPATIBILITY_ADAPTER.igniteEntity(ent, (Player) arrow.getShooter(), 300);
-                        }, 1);
+                // Imitate Flame arrows after cancelling the original event.
+                if (this.getArrow().getFireTicks() > 0) {
+                    EntityCombustByEntityEvent combustByEntityEvent = new EntityCombustByEntityEvent(this.getArrow(), entity, 5);
+
+                    Bukkit.getPluginManager().callEvent(combustByEntityEvent);
+
+                    if (!combustByEntityEvent.isCancelled()) {
+                        Bukkit.getScheduler().scheduleSyncDelayedTask(
+                            Storage.zenchantments,
+                            () -> Storage.COMPATIBILITY_ADAPTER.igniteEntity(entity, (Player) this.getArrow().getShooter(), 300),
+                            1
+                        );
                     }
                 }
 
-                ent.damage(event.getDamage());
+                entity.damage(event.getDamage());
+
                 if (event.getDamager().getType() == EntityType.ARROW) {
                     event.getDamager().remove();
                 }
             }
-
         }
-        die();
+
+        this.die();
         return true;
     }
 }
