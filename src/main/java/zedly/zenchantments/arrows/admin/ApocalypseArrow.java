86b1e939af8132ffdd9047b0a3f8ec6c5b1f156c
Louis Salkeld <louissalkeld@outlook.com>
2020-10-08 21:46:37 +0100

    Fix and clean-up ApocalypseArrow

--- src/main/java/zedly/zenchantments/arrows/admin/ApocalypseArrow.java
+++ src/main/java/zedly/zenchantments/arrows/admin/ApocalypseArrow.java
@@ -5,9 +5,9 @@ import org.bukkit.Location;
 import org.bukkit.entity.*;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.util.Vector;
-import zedly.zenchantments.Config;
 import zedly.zenchantments.Storage;
 import zedly.zenchantments.arrows.EnchantedArrow;
+import zedly.zenchantments.configuration.WorldConfiguration;
 
 import static org.bukkit.Material.FIRE;
 import static org.bukkit.entity.EntityType.BLAZE;
@@ -16,51 +16,65 @@ import static org.bukkit.potion.PotionEffectType.HARM;
 
 public class ApocalypseArrow extends EnchantedArrow {
 
-	public ApocalypseArrow(Arrow entity) {
-		super(entity);
-	}
+    public ApocalypseArrow(Arrow entity) {
+        super(entity);
+    }
 
-	public void onImpact() {
-		final Config config = Config.get(arrow.getWorld());
-		Location l2 = arrow.getLocation().clone();
-		l2.setY(l2.getY() + 1);
-		Location[] locs = new Location[]{arrow.getLocation(), l2};
-		arrow.getWorld().strikeLightning(l2);
-		for (int ls = 0; ls < locs.length; ls++) {
-			final Location l = locs[ls];
-			final int lsf = ls;
-			for (int i = 0; i <= 45; i++) {
-				final int c = i + 1;
-				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Storage.zenchantments, () -> {
-					Entity ent = l.getWorld().spawnFallingBlock(l, Bukkit.createBlockData(FIRE));
-					Vector v = l.toVector();
-					v.setY(Math.abs(Math.sin(c)));
-					if (lsf % 2 == 0) {
-						v.setZ((Math.sin(c) / 2));
-						v.setX((Math.cos(c) / 2));
-					} else {
-						v.setX((Math.sin(c) / 2));
-						v.setZ((Math.cos(c) / 2));
-					}
-					ent.setVelocity(v.multiply(1.5));
-					TNTPrimed prime = (TNTPrimed) arrow.getWorld().spawnEntity(l, EntityType.PRIMED_TNT);
-					prime.setFuseTicks(200);
-					prime.setYield(config.explosionBlockBreak() ? 4 : 0);
-					Blaze blaze = (Blaze) arrow.getWorld().spawnEntity(l, BLAZE);
-					blaze.addPotionEffect(new PotionEffect(ABSORPTION, 150, 100000));
-					blaze.addPotionEffect(new PotionEffect(HARM, 10000, 1));
-					if (config.explosionBlockBreak()) {
-						Entity crystal = arrow.getWorld().spawnEntity(l, EntityType.ENDER_CRYSTAL);
-						ent.addPassenger(prime);
-						crystal.addPassenger(blaze);
-						prime.addPassenger(crystal);
-					} else {
-						ent.addPassenger(prime);
-						prime.addPassenger(blaze);
-					}
-				}, c);
-			}
-		}
-		die();
-	}
+    public void onImpact() {
+        WorldConfiguration config = WorldConfiguration.get(this.getArrow().getWorld());
+
+        Location clone = this.getArrow().getLocation().clone();
+        clone.setY(clone.getY() + 1);
+
+        this.getArrow().getWorld().strikeLightning(clone);
+
+        Location[] locations = {this.getArrow().getLocation(), clone};
+        for (int l = 0; l < locations.length; l++) {
+            Location location = locations[l];
+            int finalLs = l;
+            for (int i = 0; i <= 45; i++) {
+                int c = i + 1;
+                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(
+                    Storage.zenchantments,
+                    () -> {
+                        Entity entity = location.getWorld().spawnFallingBlock(location, Bukkit.createBlockData(FIRE));
+
+                        Vector vector = location.toVector();
+                        vector.setY(Math.abs(Math.sin(c)));
+
+                        if (finalLs % 2 == 0) {
+                            vector.setZ((Math.sin(c) / 2));
+                            vector.setX((Math.cos(c) / 2));
+                        } else {
+                            vector.setX((Math.sin(c) / 2));
+                            vector.setZ((Math.cos(c) / 2));
+                        }
+
+                        entity.setVelocity(vector.multiply(1.5));
+
+                        TNTPrimed prime = (TNTPrimed) this.getArrow().getWorld().spawnEntity(location, EntityType.PRIMED_TNT);
+                        prime.setFuseTicks(200);
+                        prime.setYield(config.explosionBlockBreak() ? 4 : 0);
+
+                        Blaze blaze = (Blaze) this.getArrow().getWorld().spawnEntity(location, BLAZE);
+                        blaze.addPotionEffect(new PotionEffect(ABSORPTION, 150, 100000));
+                        blaze.addPotionEffect(new PotionEffect(HARM, 10000, 1));
+
+                        if (config.explosionBlockBreak()) {
+                            Entity crystal = this.getArrow().getWorld().spawnEntity(location, EntityType.ENDER_CRYSTAL);
+                            entity.addPassenger(prime);
+                            crystal.addPassenger(blaze);
+                            prime.addPassenger(crystal);
+                        } else {
+                            entity.addPassenger(prime);
+                            prime.addPassenger(blaze);
+                        }
+                    },
+                    c
+                );
+            }
+        }
+
+        this.die();
+    }
 }
