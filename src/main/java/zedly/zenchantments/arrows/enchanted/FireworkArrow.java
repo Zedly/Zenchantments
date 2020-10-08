0ef59d3923a87343bc4380219682d5d54bec08e3
Louis Salkeld <louissalkeld@outlook.com>
2020-10-08 21:14:19 +0100

    Fix and clean-up FireworkArrow

--- src/main/java/zedly/zenchantments/arrows/enchanted/FireworkArrow.java
+++ src/main/java/zedly/zenchantments/arrows/enchanted/FireworkArrow.java
@@ -1,6 +1,9 @@
 package zedly.zenchantments.arrows.enchanted;
 
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Firework;
@@ -8,29 +11,38 @@ import org.bukkit.inventory.meta.FireworkMeta;
 import zedly.zenchantments.Storage;
 import zedly.zenchantments.arrows.EnchantedArrow;
 
+import static org.bukkit.FireworkEffect.Type.*;
+
 public class FireworkArrow extends EnchantedArrow {
 
-	public FireworkArrow(Arrow entity, int level) {
-		super(entity, level);
-	}
+    public FireworkArrow(Arrow entity, int level) {
+        super(entity, level);
+    }
 
-	public void onImpact() {
-		Location l = arrow.getLocation();
-		FireworkEffect.Type[] type = {FireworkEffect.Type.BALL, FireworkEffect.Type.BURST, FireworkEffect.Type.STAR,
-			FireworkEffect.Type.BALL_LARGE};
-		FireworkEffect.Builder b = FireworkEffect.builder();
-		b = b.withColor(Color.LIME).withColor(Color.RED).withColor(Color.BLUE).withColor(Color.YELLOW).withColor(
-			Color.fromRGB(0xFF00FF)).withColor(Color.ORANGE).withColor(Color.fromRGB(0x3E89FF));
-		b = b.trail(true);
-		b = b.with(type[(getLevel() > 4 ? 4 : getLevel()) - 1]);
-		final Firework f = (Firework) l.getWorld().spawnEntity(l, EntityType.FIREWORK);
-		FireworkMeta d = f.getFireworkMeta();
-		d.setPower(0);
-		d.addEffect(b.build());
-		f.setFireworkMeta(d);
-		Bukkit.getScheduler().scheduleSyncDelayedTask(Storage.zenchantments, () -> {
-			f.detonate();
-		}, 1);
-		die();
-	}
+    public void onImpact() {
+        FireworkEffect.Type[] type = {BALL, BURST, STAR, BALL_LARGE};
+        FireworkEffect.Builder builder = FireworkEffect.builder()
+            .withColor(Color.LIME)
+            .withColor(Color.RED)
+            .withColor(Color.BLUE)
+            .withColor(Color.YELLOW)
+            .withColor(Color.fromRGB(0xFF00FF))
+            .withColor(Color.ORANGE)
+            .withColor(Color.fromRGB(0x3E89FF))
+            .trail(true)
+            .with(type[(Math.min(this.getLevel(), 4)) - 1]);
+
+        Location location = this.getArrow().getLocation();
+        Firework firework = (Firework) location.getWorld().spawnEntity(location, EntityType.FIREWORK);
+
+        FireworkMeta fireworkMeta = firework.getFireworkMeta();
+        fireworkMeta.setPower(0);
+        fireworkMeta.addEffect(builder.build());
+
+        firework.setFireworkMeta(fireworkMeta);
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(Storage.zenchantments, firework::detonate, 1);
+
+        this.die();
+    }
 }
