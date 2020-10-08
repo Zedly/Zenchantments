eca7c226c531e6b6e41e90852d366c7f6167ed2f
Louis Salkeld <louissalkeld@outlook.com>
2020-10-08 21:24:33 +0100

    Clean-up LevelArrow

--- src/main/java/zedly/zenchantments/arrows/enchanted/LevelArrow.java
+++ src/main/java/zedly/zenchantments/arrows/enchanted/LevelArrow.java
@@ -7,15 +7,15 @@ import zedly.zenchantments.arrows.EnchantedArrow;
 
 public class LevelArrow extends EnchantedArrow {
 
-	public LevelArrow(Arrow entity, int level, double power) {
-		super(entity, level, power);
-	}
+    public LevelArrow(Arrow entity, int level, double power) {
+        super(entity, level, power);
+    }
 
-	public void onKill(@NotNull EntityDeathEvent event) {
-		die(true);
-	}
-        
-        public void onImpact() {
-            die(false);
-        }
+    public void onKill(@NotNull EntityDeathEvent event) {
+        this.die(true);
+    }
+
+    public void onImpact() {
+        this.die(false);
+    }
 }
