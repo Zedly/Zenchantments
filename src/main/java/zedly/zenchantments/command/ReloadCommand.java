bd5df5d47b2e82ba931aa0290d21f213bd0ab199
Louis Salkeld <louissalkeld@outlook.com>
2020-12-12 22:31:37 +0000

    Clean-up/improvements for ReloadCommand

--- src/main/java/zedly/zenchantments/command/ReloadCommand.java
+++ src/main/java/zedly/zenchantments/command/ReloadCommand.java
@@ -9,26 +9,26 @@ import java.util.Collections;
 import java.util.List;
 
 public class ReloadCommand extends ZenchantmentsCommand {
-    public ReloadCommand(@NotNull ZenchantmentsPlugin plugin) {
+    public ReloadCommand(final @NotNull ZenchantmentsPlugin plugin) {
         super(plugin);
     }
 
     @Override
-    public void execute(@NotNull CommandSender sender, @NotNull String[] args) {
+    public void execute(final @NotNull CommandSender sender, final @NotNull String[] args) {
         if (!sender.hasPermission("zenchantments.command.reload")) {
-            sender.sendMessage(ZenchantmentsCommand.MESSAGE_PREFIX + "You do not have permission to do this!");
+            sender.sendMessage(MESSAGE_PREFIX + "You do not have permission to do this!");
             return;
         }
 
         this.plugin.getGlobalConfiguration().loadGlobalConfiguration();
         this.plugin.getWorldConfigurationProvider().loadWorldConfigurations();
 
-        sender.sendMessage(ZenchantmentsCommand.MESSAGE_PREFIX + "Reloaded Zenchantments.");
+        sender.sendMessage(MESSAGE_PREFIX + "Reloaded Zenchantments.");
     }
 
     @Override
     @Nullable
-    public List<String> getTabCompleteOptions(@NotNull CommandSender sender, @NotNull String[] args) {
+    public List<String> getTabCompleteOptions(final @NotNull CommandSender sender, final @NotNull String[] args) {
         return Collections.emptyList();
     }
 }
