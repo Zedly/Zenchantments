0df07bfdb65fe0a53f69ba5ce8006e9299556333
Louis Salkeld <louissalkeld@outlook.com>
2020-12-28 05:56:54 +0000

    Remove static initializer block from Storage

--- src/main/java/zedly/zenchantments/Storage.java
+++ src/main/java/zedly/zenchantments/Storage.java
@@ -1,25 +1,8 @@
 package zedly.zenchantments;
 
-import org.bukkit.Bukkit;
 import zedly.zenchantments.compatibility.CompatibilityAdapter;
-import zedly.zenchantments.compatibility.NMS_1_16_R1;
 
 @Deprecated
 public class Storage {
-    public static final CompatibilityAdapter COMPATIBILITY_ADAPTER;
-    static {
-        String versionString = Bukkit.getServer().getClass().getPackage().getName();
-        String nmsVersionString = versionString.substring(versionString.lastIndexOf('.') + 1);
-        System.out.println("Zenchantments: Detected NMS version \"" + nmsVersionString + "\"");
-        switch (nmsVersionString) {
-            case "v1_16_R2":
-                COMPATIBILITY_ADAPTER = NMS_1_16_R1.getInstance();
-                break;
-            default:
-                System.out.println(
-                    "No compatible adapter available, falling back to Bukkit. Not everything will work!");
-                COMPATIBILITY_ADAPTER = zedly.zenchantments.compatibility.CompatibilityAdapter.getInstance();
-                break;
-        }
-    }
+    public static final CompatibilityAdapter COMPATIBILITY_ADAPTER = null;
 }
