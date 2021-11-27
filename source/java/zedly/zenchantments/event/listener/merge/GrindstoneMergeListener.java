f5c35d2d6f597a69ecac2bd93956d6cf2b11b54d
Zedly <zedlydev@gmail.com>
2021-11-27 14:12:46 +0100

    Made it possible to work with zen-only enchanted tools in the grindstone

--- source/java/zedly/zenchantments/event/listener/merge/GrindstoneMergeListener.java
+++ source/java/zedly/zenchantments/event/listener/merge/GrindstoneMergeListener.java
@@ -1,5 +1,6 @@
 package zedly.zenchantments.event.listener.merge;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
@@ -8,13 +9,19 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.GrindstoneInventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import zedly.zenchantments.Zenchantment;
 import zedly.zenchantments.ZenchantmentsPlugin;
 import zedly.zenchantments.configuration.WorldConfiguration;
 
 import java.util.Set;
 
+import static java.util.Objects.requireNonNull;
+import static org.bukkit.Material.ENCHANTED_BOOK;
+import static org.bukkit.enchantments.Enchantment.DURABILITY;
 import static org.bukkit.event.EventPriority.MONITOR;
 
 public class GrindstoneMergeListener implements Listener {
@@ -36,11 +43,38 @@ public class GrindstoneMergeListener implements Listener {
         if (event.getSlot() == 2) {
             this.removeOutputEnchants(inventory, world);
         } else {
+            if (event.getCurrentItem() != null) {
+                final ItemMeta itemMeta = (ItemMeta) event.getCurrentItem().getItemMeta();
+                if (itemMeta != null && itemMeta.hasEnchants() && itemMeta.getEnchants().containsKey(DURABILITY)
+                    && itemMeta.getEnchants().get(DURABILITY) == 0
+                ) {
+                    itemMeta.removeEnchant(DURABILITY);
+                    event.getCurrentItem().setItemMeta(itemMeta);
+                }
+            }
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(ZenchantmentsPlugin.getInstance(), () -> {
+                ensureToolIsGrindable((GrindstoneInventory) event.getInventory(), 0);
+                ensureToolIsGrindable((GrindstoneInventory) event.getInventory(), 1);
+            }, 0);
             this.plugin.getServer().getScheduler().scheduleSyncDelayedTask(
                 this.plugin,
                 () -> this.removeOutputEnchants(inventory, world),
                 0
             );
+        }
+    }
+
+    private void ensureToolIsGrindable(final @NotNull GrindstoneInventory inventory, int slot) {
+        ItemStack item = inventory.getItem(slot);
+        if (item == null) {
+            return;
+        }
+        final ItemMeta itemMeta = (ItemMeta) item.getItemMeta();
+
+        if (itemMeta != null && (!itemMeta.hasEnchants() || !itemMeta.getEnchants().containsKey(DURABILITY))) {
+            itemMeta.addEnchant(DURABILITY, 0, true);
+            item.setItemMeta(itemMeta);
+            inventory.setItem(slot, item);
         }
     }
 
