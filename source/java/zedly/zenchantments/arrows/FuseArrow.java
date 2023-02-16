2d44f0038058db7fbca9c9390b0e391b23cf9601
Zedly <zedlydev@gmail.com>
2023-02-16 17:43:24 +0100

    Removed Fuse arrow properties which overlap with the vanilla Flame enchantment

--- source/java/zedly/zenchantments/arrows/FuseArrow.java
+++ source/java/zedly/zenchantments/arrows/FuseArrow.java
@@ -4,11 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.type.Campfire;
 import org.bukkit.block.data.type.Candle;
 import org.bukkit.entity.*;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
@@ -23,45 +27,6 @@ import static org.bukkit.Material.*;
 public final class FuseArrow extends ZenchantedArrow {
     public FuseArrow(final @NotNull AbstractArrow entity) {
         super(entity);
-    }
-
-    @Override
-    public void onImpact() {
-        final Location location = this.getArrow().getLocation();
-        for (int i = 1; i < 5; i++) {
-            final Vector vector = this.getArrow().getVelocity().multiply(0.25 * i);
-            final Location hitLocation = new Location(
-                location.getWorld(),
-                location.getX() + vector.getX(),
-                location.getY() + vector.getY(),
-                location.getZ() + vector.getZ()
-            );
-
-            if (hitLocation.getBlock().getType() == TNT) {
-                final BlockBreakEvent event = new BlockBreakEvent(
-                    hitLocation.getBlock(),
-                    (Player) Objects.requireNonNull(this.getArrow().getShooter())
-                );
-
-                Bukkit.getPluginManager().callEvent(event);
-
-                if (!event.isCancelled()) {
-                    hitLocation.getBlock().setType(AIR);
-                    hitLocation.getWorld().spawnEntity(hitLocation, EntityType.PRIMED_TNT);
-                    this.die();
-                }
-
-                return;
-            } else if (hitLocation.getBlock().getBlockData() instanceof final Candle candle) {
-                candle.setLit(true);
-                hitLocation.getBlock().setBlockData(candle);
-             } else if (hitLocation.getBlock().getBlockData() instanceof final Campfire campfire) {
-                campfire.setLit(true);
-                hitLocation.getBlock().setBlockData(campfire);
-            }
-        }
-
-        this.die();
     }
 
     @Override
