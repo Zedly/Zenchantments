a7abffc18333ff1445bf409f8c7a5b48fe47e1d7
Zedly <zedlydev@gmail.com>
2023-02-19 17:02:06 +0100

    Made Fireworks effects more diverse

--- source/java/zedly/zenchantments/arrows/FireworkArrow.java
+++ source/java/zedly/zenchantments/arrows/FireworkArrow.java
@@ -11,6 +11,8 @@ import org.bukkit.inventory.meta.FireworkMeta;
 import org.jetbrains.annotations.NotNull;
 import zedly.zenchantments.ZenchantmentsPlugin;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.bukkit.FireworkEffect.Type.*;
 
 public final class FireworkArrow extends ZenchantedArrow {
@@ -24,20 +26,35 @@ public final class FireworkArrow extends ZenchantedArrow {
     @Override
     public void onImpact() {
         final FireworkEffect.Type[] type = { BALL, BURST, STAR, BALL_LARGE };
-        final FireworkEffect.Builder builder = FireworkEffect.builder()
-            .withColor(Color.LIME)
-            .withColor(Color.RED)
-            .withColor(Color.BLUE)
-            .withColor(Color.YELLOW)
-            .withColor(Color.fromRGB(0xFF00FF))
-            .withColor(Color.ORANGE)
-            .withColor(Color.fromRGB(0x3E89FF))
-            .trail(true)
-            .with(type[(Math.min(this.getLevel(), 4)) - 1]);
+        final FireworkEffect.Builder builder = FireworkEffect.builder();
+        if(ThreadLocalRandom.current().nextBoolean()) {
+            builder.withColor(Color.LIME);
+        }
+        if(ThreadLocalRandom.current().nextBoolean()) {
+            builder.withColor(Color.RED);
+        }
+        if(ThreadLocalRandom.current().nextBoolean()) {
+            builder.withColor(Color.BLUE);
+        }
+        if(ThreadLocalRandom.current().nextBoolean()) {
+            builder.withColor(Color.ORANGE);
+        }
+        if(ThreadLocalRandom.current().nextBoolean()) {
+            builder.withColor(Color.YELLOW);
+        }
+        if(ThreadLocalRandom.current().nextBoolean()) {
+            builder.withColor(Color.fromRGB(0xFF00FF));
+        }
+        if(ThreadLocalRandom.current().nextBoolean()) {
+            builder.withColor(Color.fromRGB(0x3E89FF));
+        }
+        builder.trail(ThreadLocalRandom.current().nextBoolean())
+            .flicker(ThreadLocalRandom.current().nextBoolean())
+            .with(type[(ThreadLocalRandom.current().nextInt(Math.min(this.getLevel(), 4)))]);
 
         final Location location = this.getArrow().getLocation();
         final Firework firework = (Firework) location.getWorld().spawnEntity(location, EntityType.FIREWORK);
-
+        firework.setSilent(true);
         final FireworkMeta fireworkMeta = firework.getFireworkMeta();
         fireworkMeta.setPower(0);
         fireworkMeta.addEffect(builder.build());
